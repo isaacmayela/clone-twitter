915a79b2805c331fd05e0b861694686b8cdad8f3
isaacmayela <isaacmayela27@gmail.com>
2023-12-05 23:35:37 +0100

    feat: project migration to yarn

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Home from "./pages/home.jsx";
 import Layout from "./components/layout.jsx";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import('./style/reset.css');
 import('./style/App.css');
@@ -9,5 +10,18 @@ export default function App() {
     <Layout>
       <Home />
     </Layout>
+
+    // <Router>
+    //   <Routes>
+    //     <Route path={"/"} element={<Layout><Home /></Layout>} />
+
+    //     {/* <Route path={"/providerList"} element={<ProviderListPage/>} />
+
+    //     <Route exact path={"/ownerList"} element={<OwnerListPage/>} />
+
+    //     <Route exact path={"/companyDetails"} element={<CompanyDetailsPage/>} /> */}
+
+    //   </Routes>
+    // </Router>
   );
 }
--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,21 +1,22 @@
 import "./sidebar.css"
 import { sidebarNavigationElements } from "./navigation-elements";
 import SidebarItem from "../sidebar-item/sidebar-item";
+import { NavLink } from "react-router-dom";
 
 function Sidebar() {
     return (
-      <>
         <div className="sidebar">
+            {/* <NavLink to={"/"}> */}
             <img src="src/images/Twitter.png" alt="twitter logo" />
-        </div>
+            {/* </NavLink> */}
+            
 
-        {
+            {
             sidebarNavigationElements.map((element) =>(
                 <SidebarItem icon={element.navIcon} description={element.navText}/>
             ))
         }
-
-      </>
+        </div>
     );
   }
   
