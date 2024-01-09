b8480a25ca89d30e35361ab3ea4b2efa14aa1893
isaacmayela <isaacmayela27@gmail.com>
2024-01-09 20:46:35 +0100

    feat: i've fiched with the login page design

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import Header from '../components/header/header';
 import TweetEditor from '../components/tweet-editor/tweetEditor';
 import Tweets from '../components/tweets/tweets';
+import twitterLogo from '../images/Twitter_Logo_Blue.png'
 
 function Login() {
   return (
-    <main className="timeline">
-      <div className="login-container">
-        
+    <main className="login-container">
+      <div className="element-container">
+        <img src={twitterLogo} alt="twitter logo" />
+        <h1>Log in to Twitter</h1>
+        <input type="text" placeholder='Email'/>
+        <input type="text" placeholder='Password'/>
+        <div className='login-button-container'>
+            <a href='#'>Login</a>
+        </div>
       </div>
     </main>
   );
